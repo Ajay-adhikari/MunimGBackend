0cf85e72e8278debe1361ca9bf56bef28dc2b65a
Ajay Adhikari <ajayadhi23@gmail.com>
2025-02-10 10:36:09 +0530

    MunimGBackedn

--- src/logs/log.ts
+++ src/logs/log.ts
@@ -7,13 +7,24 @@ const dateString = new Date().toISOString().split('T')[0];
 // Create a daily log file in the 'logs' directory
 const logFilePath = path.join(__dirname, 'logs', `${dateString}.log`);
 
+// Define the logger
 const log = winston.createLogger({
   level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    })
+  ),
   transports: [
+    // File transport: Logs to a file
     new winston.transports.File({
       filename: logFilePath,
+    }),
+    // Console transport: Logs to the console
+    new winston.transports.Console({
       format: winston.format.combine(
-        winston.format.timestamp(),
+        winston.format.colorize(), // Add colors to console output
         winston.format.printf(({ timestamp, level, message }) => {
           return `${timestamp} [${level}]: ${message}`;
         })
